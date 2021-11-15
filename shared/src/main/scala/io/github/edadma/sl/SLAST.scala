ec69e4ce45c98b3b41bd0f0308a3ec6c0bdc6f2a
Edward A Maxedon Sr <edadma@gmail.com>
2021-11-15 17:38:46 -0500

    start coding parser

--- shared/src/main/scala/io/github/edadma/sl/SLAST.scala
+++ shared/src/main/scala/io/github/edadma/sl/SLAST.scala
@@ -3,3 +3,54 @@ package io.github.edadma.sl
 trait SLAST
 
 case class Ident(pos: Int, name: String)
+
+trait ExprAST extends SLAST
+
+case class InfixExprAST(left: ExprAST, op: String, right: ExprAST) extends ExprAST
+
+case class StringExpr(pos: SLParser#Position, s: String) extends ExprAST
+
+case class NumberExpr(pos: SLParser#Position, n: BigDecimal) extends ExprAST
+
+case class BooleanExpr(pos: SLParser#Position, b: Boolean) extends ExprAST
+
+case class NullExpr(pos: SLParser#Position) extends ExprAST
+
+case class VarExpr(pos: SLParser#Position, user: String, name: Ident) extends ExprAST
+
+case class ElementExpr(pos: SLParser#Position, global: String, ids: Seq[Ident]) extends ExprAST
+
+case class MapExpr(pairs: Seq[(Ident, SLParser#Position, ExprAST)]) extends ExprAST
+
+case class SeqExpr(elems: Seq[ExprAST]) extends ExprAST
+
+case class PrefixExpr(op: String, pos: SLParser#Position, expr: ExprAST) extends ExprAST
+
+case class LeftInfixExpr(lpos: SLParser#Position, left: ExprAST, right: Seq[(String, SLParser#Position, ExprAST)])
+    extends ExprAST
+
+case class RightInfixExpr(lpos: SLParser#Position, left: ExprAST, op: String, rpos: SLParser#Position, right: ExprAST)
+    extends ExprAST
+
+case class ApplyExpr(name: Ident, args: Seq[ExprAST]) extends ExprAST
+
+case class ConditionalAST(cond: ExprAST, yes: ExprAST, no: Option[ExprAST]) extends ExprAST
+
+case class OrExpr(left: ExprAST, right: ExprAST) extends ExprAST
+
+case class AndExpr(left: ExprAST, right: ExprAST) extends ExprAST
+
+case class CompareExpr(lpos: SLParser#Position, left: ExprAST, right: Seq[(String, SLParser#Position, ExprAST)])
+    extends ExprAST
+
+case class MethodExpr(expr: ExprAST, method: Ident) extends ExprAST
+
+case class IndexExpr(expr: ExprAST, pos: SLParser#Position, index: ExprAST) extends ExprAST
+
+case class PipeExpr(left: ExprAST, right: ApplyExpr) extends ExprAST
+
+case class NonStrictExpr(expr: ExprAST) extends ExprAST
+
+case class AssignmentAST(name: Ident, expr: ExprAST) extends SLAST
+
+case class ReturnAST(expr: Option[ExprAST]) extends SLAST
--- shared/src/main/scala/io/github/edadma/sl/SLParser.scala
+++ shared/src/main/scala/io/github/edadma/sl/SLParser.scala
@@ -9,6 +9,10 @@ import scala.language.implicitConversions
 
 class SLParser(val input: ParserInput) extends Parser {
 
+  class Position(val offset: Int) {
+    override def toString: String = offset.toString
+  }
+
   implicit def wsStr(s: String): Rule0 = rule(str(s) ~ sp)
 
   def kwcapture(s: String): Rule1[String] =
@@ -18,12 +22,137 @@ class SLParser(val input: ParserInput) extends Parser {
 
   def sym(s: String): Rule1[String] = rule(quiet(capture(s) ~> ((s: String) => s.trim)))
 
-  def parseExpression: ExprAST =
-    expression.run() match {
-      case Success(ast)           => ast
-      case Failure(e: ParseError) => sys.error("Expression is not valid: " + formatError(e))
-      case Failure(e)             => sys.error("Unexpected error during parsing run: " + e)
+  def expression: Rule1[ExprAST] = conditional
+
+  def conditional: Rule1[ExprAST] =
+    rule {
+      ("if" ~ condition ~ "then" ~ conditional ~ optional("else" ~ conditional) ~> ConditionalAST) | condition
     }
+
+  def condition: Rule1[ExprAST] = disjunctive
+
+  def disjunctive: Rule1[ExprAST] =
+    rule {
+      conjunctive ~ zeroOrMore("or" ~ conjunctive ~> OrExpr)
+    }
+
+  def conjunctive: Rule1[ExprAST] =
+    rule {
+      not ~ zeroOrMore("and" ~ not ~> AndExpr)
+    }
+
+  def not: Rule1[ExprAST] =
+    rule {
+      kwcapture("not") ~ pos ~ not ~> PrefixExpr |
+        comparitive
+    }
+
+  def comparitive: Rule1[ExprAST] =
+    rule {
+      pos ~ pipe ~ oneOrMore(
+        (sym("<=") | sym(">=") | sym("!=") | sym("<") | sym(">") | sym("=") | kwcapture("div")) ~
+          pos ~ pipe ~> Tuple3[String, Position, ExprAST] _) ~> CompareExpr |
+        pipe
+    }
+
+  def pipe: Rule1[ExprAST] =
+    rule {
+      applicative ~ zeroOrMore(
+        "|" ~ (apply | ident ~ push(Nil) ~> ApplyExpr) ~> PipeExpr
+      )
+    }
+
+  def applicative: Rule1[ExprAST] = rule(apply | additive)
+
+  def apply: Rule1[ApplyExpr] =
+    rule(identnsp ~ test(cursorChar != '.' && cursorChar != '[') ~ sp ~ oneOrMore(additive) ~> ApplyExpr)
+
+  def additive: Rule1[ExprAST] =
+    rule {
+      pos ~ multiplicative ~ oneOrMore((sym("++") | sym("+") | sym("-")) ~ pos ~ multiplicative ~> Tuple3[
+        String,
+        Position,
+        ExprAST] _) ~> LeftInfixExpr | multiplicative
+    }
+
+  def multiplicative: Rule1[ExprAST] =
+    rule {
+      pos ~ negative ~ oneOrMore(
+        (sym("*") | sym("/") | kwcapture("mod") | sym("\\")) ~
+          pos ~ negative ~> Tuple3[String, Position, ExprAST] _) ~> LeftInfixExpr | negative
+    }
+
+  def negative: Rule1[ExprAST] =
+    rule {
+      sym("-") ~ pos ~ negative ~> PrefixExpr |
+        power
+    }
+
+  def power: Rule1[ExprAST] =
+    rule {
+      pos ~ index ~ sym("^") ~ pos ~ power ~> RightInfixExpr |
+        index
+    }
+
+  def index: Rule1[ExprAST] =
+    rule {
+      primary ~ zeroOrMore(
+        "[" ~ pos ~ expression ~ "]" ~> IndexExpr | test(!lastChar.isWhitespace) ~ '.' ~ ident ~> MethodExpr)
+    }
+
+  def primary: Rule1[ExprAST] = rule {
+    boolean |
+      number |
+      nul |
+      variable |
+      string |
+      element |
+      map |
+      seq |
+      "`" ~ expression ~ "`" ~> NonStrictExpr |
+      "(" ~ expression ~ ")"
+  }
+
+  def map: Rule1[MapExpr] =
+    rule(
+      "{" ~ zeroOrMore(ident ~ ":" ~ pos ~ expression ~> Tuple3[Ident, Position, ExprAST] _)
+        .separatedBy(",") ~ "}" ~> MapExpr)
+
+  def seq: Rule1[SeqExpr] = rule("[" ~ zeroOrMore(expression).separatedBy(",") ~ "]" ~> SeqExpr)
+
+  def number: Rule1[NumberExpr] = rule(pos ~ decimal ~> NumberExpr)
+
+  def nul: Rule1[NullExpr] = rule(pos ~ "null" ~> NullExpr)
+
+  def boolean: Rule1[BooleanExpr] =
+    rule(pos ~ (kwcapture("true") | kwcapture("false")) ~> ((p: Position, b: String) => BooleanExpr(p, b == "true")))
+
+  def decimal: Rule1[BigDecimal] =
+    rule {
+      capture(
+        (zeroOrMore(CharPredicate.Digit) ~ '.' ~ digits | digits ~ '.') ~
+          optional((ch('e') | 'E') ~ optional(ch('+') | '-') ~ digits) |
+          digits
+      ) ~ sp ~> ((s: String) => BigDecimal(s))
+    }
+
+  def integer: Rule1[Int] = rule(capture(digits) ~ sp ~> ((s: String) => s.toInt))
+
+  def digits: Rule0 = rule(oneOrMore(CharPredicate.Digit))
+
+  def variable: Rule1[VarExpr] = rule(pos ~ capture(optional('$')) ~ ident ~> VarExpr)
+
+  def element: Rule1[ElementExpr] =
+    rule(
+      pos ~ capture(optional('$')) ~ '.' ~ zeroOrMore( /*test(!nextIsMethod) ~*/ identnsp)
+        .separatedBy('.') ~ sp ~> ElementExpr)
+
+  def string: Rule1[StringExpr] =
+    rule(pos ~ (singleQuoteString | doubleQuoteString) ~> ((p: Position, s: String) => StringExpr(p, s)))
+
+  def singleQuoteString: Rule1[String] = rule('\'' ~ capture(zeroOrMore("\\'" | noneOf("'\n"))) ~ '\'' ~ sp)
+
+  def doubleQuoteString: Rule1[String] = rule('"' ~ capture(zeroOrMore("\\\"" | noneOf("\"\n"))) ~ '"' ~ sp)
 
   def identnsp: Rule1[Ident] =
     rule {
@@ -33,6 +162,15 @@ class SLParser(val input: ParserInput) extends Parser {
 
   def ident: Rule1[Ident] = rule(identnsp ~ sp)
 
+  def pos: Rule1[Position] = rule(push(new Position(cursor)))
+
   def sp: Rule0 = rule(quiet(zeroOrMore(anyOf(" \t\r\n"))))
+
+  def parseExpression: ExprAST =
+    expression.run() match {
+      case Success(ast)           => ast
+      case Failure(e: ParseError) => sys.error("Expression is not valid: " + formatError(e))
+      case Failure(e)             => sys.error("Unexpected error during parsing run: " + e)
+    }
 
 }
